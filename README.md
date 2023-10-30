49eb8986560750271b8507865a9d87f6e03cc19f
유애나 <77397206+Teerapat28@users.noreply.github.com>
2023-10-30 18:46:13 +0700

    grocery-bud by default is a Checklist today CRUD Local Storage React-Toastify

---
+++ README.md
@@ -0,0 +1,4 @@
+## Figma URL
+
+[Grocery Bud](https://www.figma.com/file/8rXGl68NoEmAhHpcV7aB5o/Grocery-bud?node-id=0%3A1&t=IMjjwDExGWpXdpQL-1)
+
---
+++ src/SingleItem.jsx
@@ -0,0 +1,28 @@
+const SingleItem = ({ item, removeItem, editItem }) => {
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => editItem(item.id)}
+      />
+      <p
+        style={{
+          textDecoration: item.completed && "line-through",
+          textTransform: "capitalize",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
+};
+
+export default SingleItem;
